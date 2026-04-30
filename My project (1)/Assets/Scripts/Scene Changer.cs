6db9f0e2ad20f6d06bce4253028e8d637aacd747
kxcastilliano <kxcastil@uci.edu>
2026-04-29 18:50:15 -0700

    TimeLine Cutscenes (PlaceHolder)

---
+++ My project (1)/Assets/Scripts/Scene Changer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public float changetime;
+    public string scenename;
+    void Update()
+    {
+        changetime -= Time.deltaTime;
+        if (changetime < 0)
+        { SceneManager.LoadScene(scenename); }
+    }
+}
